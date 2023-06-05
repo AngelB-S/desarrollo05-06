83cbd29da725b4d20131f7d220285d20ea6257fe
SCIS3-PC07\SCIS3-07 <colombia@hotmail.com>
2023-06-05 12:23:12 -0500

    push inicial (backup)

---
+++ vue-project/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import cardComponent from '@/components/cardComponent.vue'
+const users = [
+  {
+    name: 'Juana de arco',
+    mail: 'juana@mail.com'
+  },
+  {
+    name: 'Fulano de Tal',
+    mail: 'fulano@mail.com'
+  }
+]
+
+</script>
+
+<template>
+  <h1 class="text-2xl"> Hola mundo</h1>
+
+  <cardComponent v-for="(user, index) in users" :key="index" :name="user.name" :mail="user.mail" />  
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/cardComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+const mensaje = () => {
+    alert('Dio click en la foto')
+}
+const props = defineProps({
+  name : {
+    type: String,
+    default: 'No name'
+  },
+  mail : {
+    type: String,
+    default: 'No mail'
+  }
+})
+</script>
+
+<template>
+    <div class="w-48 rounded shadow overflow-hidden">
+        <div class="inline-block align-top" @click="mensaje">
+            <img src="https://picsum.photos/40" alt="">
+        </div>
+        <div class="inline-block pl-2">
+            <h2 class="text-base font-semibold">{{ props.name }}</h2>
+            <p class="text-xs">{{ props.mail }}</p>
+        </div>
+    </div>
+</template>
