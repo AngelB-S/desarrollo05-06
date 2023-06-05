778152ace31bedddbdc759cc2ece23046626e978
SCIS3-PC07\SCIS3-07 <colombia@hotmail.com>
2023-06-05 13:09:40 -0500

    push backup

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import cardComponent from '@/components/cardComponent.vue'
-const users = [
+import { ref } from 'vue';
+const users = ref([
   {
     name: 'Juana de arco',
     mail: 'juana@mail.com'
@@ -9,14 +10,38 @@ const users = [
     name: 'Fulano de Tal',
     mail: 'fulano@mail.com'
   }
-]
+])
+
+const borrarTarjeta = (index) => {
+  console.log('KILL to', index)
+  users.value.splice(index, 1)
+  console.log(users)
+   
+}
+
+let name, mail
+
+const addUser = () => {
+  const newUser = {
+    name,
+    mail
+  }
+  users.value.push(newUser)
+}
 
 </script>
 
 <template>
   <h1 class="text-2xl"> Hola mundo</h1>
+  <input type="text" v-model="name" placeholder="Nombre" class="border rounded"> <br> <br>
+  <input type="text" v-model="mail" placeholder="Correo" class="border rounded">
+  <br>
+  <button class="bg-green-200 px-2 py-1 rounded mt-2" @click="addUser">Añadir</button>
 
-  <cardComponent v-for="(user, index) in users" :key="index" :name="user.name" :mail="user.mail" />  
+  <div class="space-y-2">
+    <cardComponent v-for="(user, index) in users" :key="index" :name="user.name" 
+    :mail="user.mail" :index="index" @borrarTarjeta="borrarTarjeta"/>  
+  </div>
 </template>
 
 <style scoped>
--- vue-project/src/components/cardComponent.vue
+++ vue-project/src/components/cardComponent.vue
@@ -2,26 +2,41 @@
 const mensaje = () => {
     alert('Dio click en la foto')
 }
+
 const props = defineProps({
-  name : {
+  name: {
     type: String,
     default: 'No name'
   },
-  mail : {
+  mail: {
     type: String,
     default: 'No mail'
+  },
+  index: {
+    type: Number
   }
 })
+
+const borrarTarjeta = (index) =>{
+  console.log('Borrar tarjeta', index)
+  emit('borrarTarjeta', index)
+}
+
+const emit = defineEmits(['borrarTarjeta'])
+
 </script>
 
 <template>
     <div class="w-48 rounded shadow overflow-hidden">
-        <div class="inline-block align-top" @click="mensaje">
+        <div class="inline-block align-top" @click="mensaje" >
             <img src="https://picsum.photos/40" alt="">
         </div>
         <div class="inline-block pl-2">
             <h2 class="text-base font-semibold">{{ props.name }}</h2>
             <p class="text-xs">{{ props.mail }}</p>
         </div>
+        <span class="inline-block ml-3 bg-red-400 text-white rounded w-6 text-center hover:cursor-pointer hover:bg-red-600" @click="borrarTarjeta(props.index)">
+            X
+        </span>
     </div>
 </template>
